47d4bf02cb57143519488b719c39b0ed70074845
arnitamartiana <arnita.martiana92@gmail.com>
2017-06-14 20:34:51 +0700

    commiit repo
    pindah repository

---
+++ Assets/EnemyLogic.js
@@ -0,0 +1,16 @@
+#pragma strict
+var Health = 100 ;
+
+
+function ApplyDammage (Damage : int)
+  { 
+    Health -= Damage ;
+    if (Health < 0)
+    {
+      Dead();
+    }
+  }
+function Dead()
+  {
+    Destroy (gameObject);
+  }
